3176a1fde6eff167e03b7d86422cf9a29f91c97b
Ming_DESKTOP <etgohome8@gmail.com>
2022-04-28 15:09:43 +0900

    0428-1508

---
+++ src/main/java/three/aws/wo/admin/util/MessageSend.java
@@ -0,0 +1,53 @@
+package three.aws.wo.admin.util;
+
+import java.util.HashMap;
+
+import org.json.simple.JSONObject;
+
+import net.nurigo.java_sdk.api.Message;
+import net.nurigo.java_sdk.exceptions.CoolsmsException;
+
+public class MessageSend {
+	private String api_key = "NCSIGXZPTDLXMH7U";
+	private String api_secret = "NZKJLXJYUTDZIAC9KLTQB2GYZWZUMZAG";
+	private String fromNum ="01027764122";
+	
+	public void sendSMS(String toNum, String content) {
+		Message coolsms = new Message(api_key, api_secret);
+
+		// 4 params(to, from, type, text) are mandatory. must be filled
+		HashMap<String, String> params = new HashMap<String, String>();
+		params.put("to", toNum);
+		params.put("from", fromNum);
+		params.put("type", "SMS");
+		params.put("text", content);
+//		params.put("app_version", "test app 1.2"); // application name and version
+
+		try {
+			JSONObject obj = (JSONObject) coolsms.send(params);
+			System.out.println(obj.toString());
+		} catch (CoolsmsException e) {
+			System.out.println(e.getMessage());
+			System.out.println(e.getCode());
+		}
+	}
+	
+	public void sendLMS(String toNum, String content) {
+	    Message coolsms = new Message(api_key, api_secret);
+	    // 4 params(to, from, type, text) are mandatory. must be filled
+	    HashMap<String, String> params = new HashMap<String, String>();
+	    params.put("to", toNum);
+	    params.put("from", fromNum);
+	    params.put("type", "LMS");
+	    params.put("text", content);
+//	    params.put("app_version", "test app 1.2"); // application name and version
+
+	    try {
+	      JSONObject obj = (JSONObject) coolsms.send(params);
+	      System.out.println(obj.toString());
+	    } catch (CoolsmsException e) {
+	      System.out.println(e.getMessage());
+	      System.out.println(e.getCode());
+	    }
+	  }
+}
---
+++ src/main/java/three/aws/wo/admin/vo/MessageVO.java
@@ -0,0 +1,10 @@
+package three.aws.wo.admin.vo;
+
+import lombok.Data;
+
+@Data
+public class MessageVO {
+	private String sms_;
+	private String sms_title;
+	private String sms_text;
+}

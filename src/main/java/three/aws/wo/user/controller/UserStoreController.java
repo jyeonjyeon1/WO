59c24def1c007a201471323749efd17810e9c857
jyeonjyeon1 <n8650n@naver.com>
2022-05-12 01:32:41 +0900

    aa

--- src/main/java/three/aws/wo/user/controller/UserStoreController.java
+++ src/main/java/three/aws/wo/user/controller/UserStoreController.java
@@ -1,72 +1,41 @@
 package three.aws.wo.user.controller;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import javax.annotation.Resource;
 
 import org.apache.maven.shared.invoker.SystemOutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import three.aws.wo.user.service.UStoreService;
-import three.aws.wo.user.vo.UserVO;
 import three.aws.wo.store.vo.StoreVO;
 
 @Controller
 public class UserStoreController {
 	@Resource
 	private UStoreService uStoreService;
-	private String SearchWord; //검색어 저장 할 아이
-	private List<StoreVO> storeListByPage; //페이징처리를위한 list공유
 	
-	//store 검색기능
 	@RequestMapping("/storeList.user")
-	public String storeList(StoreVO vo, Model model) { 
-		storeListByPage =uStoreService.storeListByPage(SearchWord);
+	public String storeList(StoreVO vo, Model model) {
+		List<StoreVO> storeListByPage =uStoreService.storeListByPage();
 		System.out.println("user에서 storelist 열었다!");
-		System.out.println(SearchWord);
-		model.addAttribute("userPageChange" ,storeListByPage);
+		model.addAttribute("storeListByPage" ,storeListByPage);
 		return "/order/order_storeList";
 	}
 	
-	//header에서 검색어 받아오기
-	@ResponseBody
-	@RequestMapping(value="/searchStore.user", method = RequestMethod.POST)
-	public String searchStore(@RequestBody HashMap<String, String> param ) {
-		SearchWord = param.get("Sname");
-		//System.out.println(SearchWord);
-		return "redirect:storeList.user";
-	}
-	
-	//검색기능 - 페이징 처리
 	@ResponseBody
 	@RequestMapping(value = "/pagingProcess.user", method = RequestMethod.POST)
-	public List<StoreVO> UserPageChange(int pageNum, Model model) {
-		int startPageNum=1;
-		if(pageNum==0) {
-			startPageNum=1;
-		}
+	public String UserPageChange(int pageNum, Model model) {
 		
-		startPageNum = (pageNum*10) - 9;
-		int numberInOnePage =10;
-		int endPageNum = startPageNum +numberInOnePage-1;
-		System.out.println("PageNum:" + pageNum + ", startPageNum: " + startPageNum );
-		
-		HashMap<String, Integer> map = new HashMap<String,Integer>();
-		map.put("startPageNum", startPageNum);
-		map.put("endPageNum", endPageNum);
-		
-		//List<StoreVO> userPageChange =uStoreService.UserPageChange(map);
-		//model.addAttribute("userPageChange" ,userPageChange);
-		return storeListByPage;
+		int startPageNum = (pageNum*10) - 9;
+		System.out.println("PageNum:" + pageNum + ", startPageNum: " + startPageNum);
+		List<StoreVO> userPageChange =uStoreService.UserPageChange(startPageNum);
+		model.addAttribute("userPageChange",userPageChange);
+		return "/order/order_storeList";
 	}
-	
-	
 	
 }

b903613370f7dae4f08d524a5905d2aef7e3839b
Ming <etgohome8@gmail.com>
2022-05-28 11:09:03 +0900

    0528-1108 getmapping 누락

--- src/main/java/three/aws/wo/admin/controller/AdminPageController.java
+++ src/main/java/three/aws/wo/admin/controller/AdminPageController.java
@@ -8,6 +8,7 @@ import javax.inject.Inject;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
